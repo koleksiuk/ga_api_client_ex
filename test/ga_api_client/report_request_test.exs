2030dbfa5f98b77e0efad543c34fb3b601ce5149
Konrad Oleksiuk <konole@gmail.com>
2017-05-07 12:45:23 +0200

    Add test for complex request build

--- test/ga_api_client/report_request_test.exs
+++ test/ga_api_client/report_request_test.exs
@@ -1,4 +1,19 @@
 defmodule GaApiClient.ReportRequestTest do
   use ExUnit.Case
   doctest GaApiClient.ReportRequest
+
+  alias GaApiClient.ReportRequest
+
+  test "it is possible to merge metrics and dimensions" do
+    report_request = ReportRequest.build("1234")
+                   |> ReportRequest.dimensions([:city, :country])
+                   |> ReportRequest.metrics([:pageviews])
+
+    assert report_request == %GaApiClient.ReportRequest {
+      view_id: "1234",
+      dimensions: [:city, :country],
+      metrics: [:pageviews]
+    }
+
+  end
 end
