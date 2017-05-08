418658f290f1ce93e3e1acefd934324c79719b88
Konrad Oleksiuk <konole@gmail.com>
2017-05-08 22:07:41 +0200

    Remove boilerplate code

--- lib/ga_api_client.ex
+++ lib/ga_api_client.ex
@@ -1,18 +1,2 @@
 defmodule GaApiClient do
-  @moduledoc """
-  Documentation for GaApiClient.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> GaApiClient.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/ga_api_client_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule GaApiClientTest do
-  use ExUnit.Case
-  doctest GaApiClient
-
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
-end
